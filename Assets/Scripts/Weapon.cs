1d96990c22713a29b49be0cdc503a776ac63a83d
GalambosAbel <55552354+GalambosAbel@users.noreply.github.com>
2019-10-18 21:20:12 +0200

    small tweaks

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -10,7 +10,7 @@ public class Weapon : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
             
