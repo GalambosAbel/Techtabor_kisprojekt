351834bfe4dac43039b68ff428913d8257628462
mtotbagi <mtotbagi@users.noreply.github.com>
2019-10-19 15:15:35 +0200

    Score

---
+++ Assets/Score.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public int score;
+
+
+    void Update()
+    {
+        if(score < Player.p.playerOne.transform.position.y)
+        {
+            score = Mathf.RoundToInt(Player.p.playerOne.transform.position.y);
+        }
+        GetComponent<Text>().text = (score-111).ToString();
+    }
+}
