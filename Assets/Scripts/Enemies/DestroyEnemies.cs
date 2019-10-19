2d6d19112574f7c2ebaadd248293ae157f528242
mtotbagi <mtotbagi@users.noreply.github.com>
2019-10-19 11:55:35 +0200

    Enemy destroy on checkpoint

---
+++ Assets/Scripts/Enemies/DestroyEnemies.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEnemies : MonoBehaviour
+{
+    public void KillEnemies()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Destroy(transform.GetChild(0).gameObject);
+        }
+    }
+}
--- Assets/Scripts/passive/LevelGenerator.cs
+++ Assets/Scripts/passive/LevelGenerator.cs
@@ -38,6 +38,7 @@ public class LevelGenerator : MonoBehaviour
 	{
 		GetComponent<EnemySpawner>().StopSpawn();
 		GetComponent<CameraMovment>().Stop();
+        FindObjectOfType<DestroyEnemies>().KillEnemies();
 		float y = transform.position.y - Camera.main.orthographicSize + background.transform.localScale.y / 200;
 		background.transform.position = new Vector3(transform.position.x, y, transform.position.z);
 		for (int i = 0; i < checkPointHeight - 1; i++)
