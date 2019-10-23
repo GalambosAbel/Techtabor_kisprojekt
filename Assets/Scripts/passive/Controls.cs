bc1e6d463877fc4d90d1fedb55c358b12b43db46
mtotbagi <mtotbagi@users.noreply.github.com>
2019-10-23 10:58:16 +0200

    Controls

---
+++ Assets/Scripts/passive/Controls.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPref : MonoBehaviour
+{
+    void Start()
+    {
+        PlayerPrefs.SetInt("Jump", (int)KeyCode.Space);
+        PlayerPrefs.SetInt("Left", (int)KeyCode.A);
+        PlayerPrefs.SetInt("Right", (int)KeyCode.D);
+        PlayerPrefs.SetInt("Shoot", (int)KeyCode.Mouse0);
+    }
+}
---
+++ Assets/Scripts/passive/PlayerPref.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controls : MonoBehaviour
+{
+    public Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>();
+    public Text jump, left, right, shoot;
+
+    void Start()
+    {
+        keys.Add("Jump", (KeyCode)PlayerPrefs.GetInt("Jump"));
+        keys.Add("Left", (KeyCode)PlayerPrefs.GetInt("Left"));
+        keys.Add("Right", (KeyCode)PlayerPrefs.GetInt("Right"));
+        keys.Add("Shoot", (KeyCode)PlayerPrefs.GetInt("Shoot"));
+        jump.text = keys["Jump"].ToString();
+        left.text = keys["Left"].ToString();
+        right.text = keys["Right"].ToString();
+        shoot.text = keys["Shoot"].ToString();
+    }
+
+}
