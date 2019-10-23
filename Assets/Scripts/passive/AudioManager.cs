f2f02600da0d5ef596b68ee48f08edd24b0c2d99
GalambosAbel <55552354+GalambosAbel@users.noreply.github.com>
2019-10-23 12:11:17 +0200

    Added AudioManager

---
+++ Assets/Scripts/passive/AudioManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+	public Sound[] sounds;
+
+	void Awake()
+	{
+		foreach (Sound s in sounds)
+		{
+			s.source = gameObject.AddComponent<AudioSource>();
+
+			s.source.clip = s.clip;
+			s.source.volume = s.volume;
+			s.source.loop = s.loop;
+		}
+	}
+
+	void Start()
+    {
+        
+    }
+
+	public void Play (string clipName)
+	{
+		Sound s = Array.Find(sounds, sound => sound.name == clipName);
+		if (s == null)
+		{
+			Debug.LogWarning("missing sound: '" + clipName + "'");
+			return;
+		}
+		s.source.Play();
+	}
+}
