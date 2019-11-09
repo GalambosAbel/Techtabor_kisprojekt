9c334545fb9ccad61d3ad5406ac073149af30afd
mtotbagi <mtotbagi@users.noreply.github.com>
2019-11-09 21:58:39 +0100

    VolumeFix

--- Assets/Scripts/passive/Volume.cs
+++ Assets/Scripts/passive/Volume.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Volume : MonoBehaviour
 {
+    void Awake()
+    {
+        GetComponentInChildren<Slider>().value = PlayerPrefs.GetFloat("volume", 1f);
+    }
+
 	public void SetVolume (float volume)
 	{
 		PlayerPrefs.SetFloat("volume", volume);
